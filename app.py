744e1809b3d96813819129655f14806a5907005c
Lyndsey28 <38457304+Lyndsey28@users.noreply.github.com>
2022-10-10 16:14:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,9 @@ SHOW_DIALOG_str = str(SHOW_DIALOG_bool).lower()
 
 @app.route('/',methods=['GET'])
 def you_got_me():
-    target = os.environ.get('TARGET', 'World')
-    return 'Hello {}!\n'.format(target)
+    #target = os.environ.get('TARGET', 'World')
+    #return 'Hello {}!\n'.format(target)
+    return 'Hello world, this is Lyndsey!'
 
 #if we receive a message POST from slack
 
